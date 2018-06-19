a2ff9f9f0138929b7d30ec0ff20f05fbdafc7cce
Park Munchan <kaki104@hanmail.net>
2018-06-19 23:17:19 +0900

    Multi-Instance UWP app

---
+++ KakiViewer/App.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// This code is almost identical to the Blank Application code.
+// The key difference is that this project includes the declaration of 
+// SupportsMultipleInstances in the package.appxmanifest.
+
+namespace KakiViewer
+{
+    sealed partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            Suspending += OnSuspending;
+        }
+        /// <summary>
+        /// 파일로 엑티베이션될 때
+        /// </summary>
+        /// <param name="args"></param>
+        protected override void OnFileActivated(FileActivatedEventArgs args)
+        {
+            if (!(Window.Current.Content is Frame rootFrame))
+            {
+                rootFrame = new Frame();
+                Window.Current.Content = rootFrame;
+            }
+            if (rootFrame.Content == null)
+            {
+                var parameter = string.Empty;
+                if (args.Files.Any())
+                    parameter = args.Files[0].Path;
+                rootFrame.Navigate(typeof(MainPage), parameter);
+            }
+            Window.Current.Activate();
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            if (!(Window.Current.Content is Frame rootFrame))
+            {
+                rootFrame = new Frame();
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: Load state from previously suspended application.
+                }
+                Window.Current.Content = rootFrame;
+            }
+            if (e.PrelaunchActivated == false)
+            {
+                if (rootFrame.Content == null)
+                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                Window.Current.Activate();
+            }
+        }
+
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Save application state and stop any background activity.
+            deferral.Complete();
+        }
+    }
+}
---
+++ KakiViewer/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using Windows.Storage;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+namespace KakiViewer
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (!(e.Parameter is string filePath) || string.IsNullOrEmpty(filePath)) return;
+
+            ApplicationView.GetForCurrentView().Title = filePath;
+
+            var firstFile = await StorageFile.GetFileFromPathAsync(filePath);
+            if (firstFile == null) return;
+
+            using (var stream = await firstFile.OpenReadAsync())
+            {
+                var bi = new BitmapImage();
+                bi.SetSource(stream);
+                Image.Source = bi;
+            }
+        }
+    }
+}
