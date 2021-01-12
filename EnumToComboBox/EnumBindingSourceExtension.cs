e40079fbd012dbe28ac3b51258928a38db5f1361
Connor Park <kaki104@hanmail.net>
2021-01-12 20:22:25 +0900

    add EnumToComboBox

---
+++ EnumToComboBox/EnumBindingSourceExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Windows.UI.Xaml.Markup;
+
+namespace EnumToComboBox
+{
+    public class EnumBindingSourceExtension : MarkupExtension
+    {
+        public Type EnumType { get; set; }
+
+        public EnumBindingSourceExtension()
+        {
+        }
+
+        protected override object ProvideValue()
+        {
+            if (EnumType is null || !EnumType.IsEnum)
+                throw new Exception("EnumType must not be null and of type enum");
+            return Enum.GetValues(EnumType);
+        }
+    }
+}
